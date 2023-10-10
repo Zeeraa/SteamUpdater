f0c8817a98d5eec1d2ef6fa3ab9df9e3e28bc214
Anton udén <udenanton@hotmail.com>
2023-10-10 11:20:02 +0200

    initial electron app layout

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,25 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-}
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
+	const [test, setTest] = useState<string>('');
+
+	async function send() {
+		const resp = await axios.get('https://wtfismyip.com/json');
+		setTest(JSON.stringify(resp.data));
+	}
+
+	useEffect(() => {
+		send();
+	}, []);
+
+	return (
+		<div>
+			<h1 className="text-danger">Hello World!</h1>
+			{test}
+		</div>
+	);
 }
