d24e2664359a3e5948826dca2424f28ce9ea8d4c
Anton udén <udenanton@hotmail.com>
2023-10-14 01:43:55 +0200

    Update LogOutput.tsx

--- src/renderer/components/log/LogOutput.tsx
+++ src/renderer/components/log/LogOutput.tsx
@@ -9,7 +9,6 @@ import { WebglAddon } from 'xterm-addon-webgl';
 import "xterm/css/xterm.css";
 import { LogType } from '../../../shared/LogType';
 import { ConsoleColor } from '../../../shared/ConsoleColor';
-import toast from 'react-hot-toast';
 
 interface Props {
 	className?: string;
@@ -95,15 +94,11 @@ export default function LogOutput({ className }: Props) {
 		updateSize();
 
 		return () => {
-			try {
+			console.log("Disposing XTerm");
 			fit.dispose();
 			webgl.dispose();
 			terminal.dispose();
-			} catch(err) {
-				console.error("An error occured while disposing xterm");
-				console.error(err);
-				toast.error("An internal error occured while closing log window");
-			}
+			console.log("XTerm disposed");
 			steamUpdater.events.off(SteamUpdaterFrontendEvent.LOGS_FETCHED, handleLogs);
 			steamUpdater.events.off(SteamUpdaterFrontendEvent.LOG_MESSAGE, handleLog);
 			window.removeEventListener('resize', updateSize);
