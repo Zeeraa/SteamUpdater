0070ca2f1c66c2519b550831dfebebb468a8ef40
Anton udén <udenanton@hotmail.com>
2023-10-14 00:01:30 +0200

    make buttons less dumb

--- src/renderer/components/buttons/StartLoginTestButton.tsx
+++ src/renderer/components/buttons/StartLoginTestButton.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { useSteamUpdater } from '../../context/SteamUpdaterContext'
+import SteamUpdaterState, { State } from '../../../shared/SteamUpdaterState';
+import { SteamUpdaterFrontendEvent } from '../../script/SteamUpdaterFrontend';
 
 interface Props {
 	className?: string
@@ -8,12 +10,39 @@ interface Props {
 
 export default function StartLoginTestButton({ className }: Props) {
 	const steamUpdater = useSteamUpdater();
+	const [state, setState] = useState<SteamUpdaterState>(steamUpdater.state);
+
+	useEffect(() => {
+		const handleStateChange = (state: SteamUpdaterState) => {
+			setState(state);
+		}
+		steamUpdater.events.on(SteamUpdaterFrontendEvent.STATE_UPDATE, handleStateChange);
+		return () => {
+			steamUpdater.events.off(SteamUpdaterFrontendEvent.STATE_UPDATE, handleStateChange);
+		};
+	}, []);
 
 	function triggerLoginTest() {
 		steamUpdater.triggerLoginTest();
 	}
 
+	function isReady() {
+		if(state.loginTestRunning) {
+			return false;
+		}
+
+		if(!state.steamcmdInstalled) {
+			return false;
+		}
+
+		if(state.state == State.UPDATING) {
+			return false;
+		}
+
+		return true;
+	}
+
 	return (
-		<Button variant='info' className={className} onClick={triggerLoginTest}>Test logins</Button>
+		<Button variant='info' className={className} onClick={triggerLoginTest} disabled={!isReady()}>Test logins</Button>
 	)
 }
--- src/renderer/components/buttons/StartUpdateButton.tsx
+++ src/renderer/components/buttons/StartUpdateButton.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { useSteamUpdater } from '../../context/SteamUpdaterContext'
+import SteamUpdaterState, { State } from '../../../shared/SteamUpdaterState';
+import { SteamUpdaterFrontendEvent } from '../../script/SteamUpdaterFrontend';
 
 interface Props {
 	className?: string
@@ -8,12 +10,23 @@ interface Props {
 
 export default function StartUpdateButton({ className }: Props) {
 	const steamUpdater = useSteamUpdater();
+	const [state, setState] = useState<SteamUpdaterState>(steamUpdater.state);
+
+	useEffect(() => {
+		const handleStateChange = (state: SteamUpdaterState) => {
+			setState(state);
+		}
+		steamUpdater.events.on(SteamUpdaterFrontendEvent.STATE_UPDATE, handleStateChange);
+		return () => {
+			steamUpdater.events.off(SteamUpdaterFrontendEvent.STATE_UPDATE, handleStateChange);
+		};
+	}, []);
 
 	function triggerUpdate() {
 		steamUpdater.startUpdate();
 	}
 
 	return (
-		<Button variant='primary' className={className} onClick={triggerUpdate}>Run update</Button>
+		<Button variant='primary' className={className} onClick={triggerUpdate} disabled={state.state != State.READY}>Run update</Button>
 	)
 }
